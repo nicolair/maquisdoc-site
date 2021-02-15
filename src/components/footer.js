916f925ab8e2ba851bab1ae90f7dc7efce1c1369
Rémy <nicolair@maquisdoc.net>
2021-02-15 07:27:35 +0100

    Ajout d'un footer (merci à Jufevic)  Sur la branche main  Votre branche est à jour avec 'origin/main'.
     Modifications qui seront validées :
    	modifié :         package-lock.json
    	modifié :         package.json
    	nouveau fichier : src/components/footer.js
    	modifié :         src/components/layout.js

---
+++ src/components/footer.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import { css } from "@emotion/core"
+import { makeStyles } from '@material-ui/core/styles'
+//import Paper from '@material-ui/core/Paper'
+import Grid from "@material-ui/core/Grid"
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+
+import { useStaticQuery, Link, graphql } from 'gatsby'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: '#CADFC8',
+  },
+  sitemap: {
+    marginBottom: theme.spacing(2),
+    fontSize: "15px",
+  },
+  navtitle: {
+    fontWeight: 700,
+  },
+  copyright: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
+function ListTitleLink(props) {
+  const classes = useStyles();
+
+  return <ListItemLink className={classes.navtitle} {...props} />;
+}
+
+export default function Footer() {
+  const classes = useStyles();
+  const data = useStaticQuery(graphql`
+    query FooterQuery {
+      allMarkdownRemark(filter: {frontmatter: {theme: {eq: "développement"}}}, sort: {fields: frontmatter___rang, order: ASC}) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+      allMenuVuesCsv {
+        edges {
+          node {
+            code
+            page
+            texte
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <Box
+      className={classes.root}
+      width={'100%'}
+      paddingX={{ xs: 2, sm: 3, lg: 4 }}
+      component='footer'
+    >
+      <Divider />
+      <Grid container className={classes.copyright}>
+        <Grid item xs={12} sm={6}>
+          <Box py={1} textAlign={{ xs: 'center', sm: 'left' }}>
+            <Typography
+              component={'p'}
+              variant={'caption'}
+              color={'textSecondary'}
+            >
+              Fait avec 
+              <Link to="https://www.gatsbyjs.com/"> Gatsby</Link>, 
+              <Link to="https://graphql.org/"> GraphQL</Link>, 
+              <Link to="https://neo4j.com/"> Neo4j</Link> 
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box py={1} textAlign={{ xs: 'center', sm: 'right' }}>
+            <Typography
+              component={'p'}
+              variant={'caption'}
+              color={'textSecondary'}
+            >
+              © {data.site.siteMetadata.title} {new Date().getFullYear()}
+            </Typography>
+            <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" css={css`
+              color: darkgreen;
+            `}>
+              <img alt="Licence Creative Commons" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by.svg" />
+            </a>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import { css } from "@emotion/core"
 import { useStaticQuery, Link, graphql } from "gatsby"
 
 import { rhythm } from "../utils/typography"
+import Footer from "./footer"
 
 export default function Layout({ children }) {
     const data = useStaticQuery(
@@ -20,55 +21,57 @@ export default function Layout({ children }) {
     `
   )
   return (
-    <div
-      css={css`
-        margin: 0 auto;
-        max-width: 700px;
-        padding: ${rhythm(2)};
-        padding-top: ${rhythm(1.5)};
-      `}
-    >
-      <div >
-        <Link to={`/`}>
-          <h1
-            css={css`
-              margin-bottom: ${rhythm(2)};
-              display: inline-block;
-              font-style: normal;
-              color: darkgreen;
-            `}
-          >
-            {data.site.siteMetadata.title}
-          </h1>
-        </Link>
-        <ul css={css`
-                float: right;
-                list-style-type: none;
-                `}> 
-            <div>
-              <Link 
-                css={css`
-                    color: darkgreen;
-                    `}
-                to= "/developpement"
-              >
-                Développement
-              </Link>
-            </div>
-            <div>
-              <Link
-                css={css`
-                    color: darkgreen;
-                    `}
-                to= "/vues"
-              >
-                Vues
-              </Link>
-            </div>
-        </ul>
-      </div>
-    {children}
+    <div>
+      <div
+        css={css`
+          margin: 0 auto;
+          max-width: 700px;
+          padding: ${rhythm(2)};
+          padding-top: ${rhythm(1.5)};
+        `}
+      >
+          <Link to={`/`}>
+            <h1
+              css={css`
+                margin-bottom: ${rhythm(2)};
+                display: inline-block;
+                font-style: normal;
+                color: darkgreen;
+              `}
+            >
+              {data.site.siteMetadata.title}
+            </h1>
+          </Link>
+          <ul css={css`
+                  float: right;
+                  list-style-type: none;
+                  `}> 
+              <div>
+                <Link 
+                  css={css`
+                      color: darkgreen;
+                      `}
+                  to= "/developpement"
+                >
+                  Développement
+                </Link>
+              </div>
+              <div>
+                <Link
+                  css={css`
+                      color: darkgreen;
+                      `}
+                  to= "/vues"
+                >
+                  Vues
+                </Link>
+              </div>
+          </ul>
+        {children}
+        </div>
+      <Footer />
     </div>
+    
   )
 }
 
